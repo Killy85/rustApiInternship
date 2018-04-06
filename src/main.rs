542c67d33bba0464a077bfc6e230f38917201c7e
Killy85 <goureau.n.a@hotmail.fr>
2018-04-06 16:07:30 +0200

    Addding a test route for the database
    TODO : Create connection in a higher scope. Return JSON object according to db content

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,17 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate postgres;
 
+use postgres::{Connection, TlsMode};
 use rocket::http::RawStr;
 use rocket::response::content;
+
+
+struct Internship {
+    id: i32,
+    title: String
+}
 
 #[get("/")]
 fn hello() -> &'static str {
@@ -22,6 +30,22 @@ fn vntm(name: &RawStr) -> content::Html<String> {
     content::Html(format!("<!DOCTYPE html><html><body><h1 style=\"text-decoration: blink\">Va bien niquer ta mère {} !!!</h1></body></html>", name.as_str()))
     }
 
+#[get("/test-db")]
+fn test_db() -> &'static str {
+    let conn = Connection::connect("postgres://wjdpqrrq:kxYU23ThjIOSmtVqi6lX4BpSUdQXMG7e@horton.elephantsql.com:5432/wjdpqrrq",
+                               TlsMode::None).unwrap();
+    for row in &conn.query("SELECT id, title FROM internship", &[]).unwrap() {
+        let person = Internship {
+            id: row.get(0),
+            title: row.get(1),
+        };
+        println!("Found person {}  {}",person.id, person.title);
+    }
+    "oui"
+    }
+
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![hello, helloname, vntm]).launch();
+    rocket::ignite().mount("/", routes![hello, helloname, vntm, test_db]).launch();
 } 
