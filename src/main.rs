49b62c7b7f8d4f0822a273fb255c12dce942cf02
mathiasLoiret <mathias44.loiret@hotmail.fr>
2018-04-06 15:31:23 +0200

    modif Cargooml

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,12 @@ fn vntm(name: &RawStr) -> content::Html<String> {
     content::Html(format!("<!DOCTYPE html><html><body><h1 style=\"text-decoration: blink\">Va bien niquer ta mère {} !!!</h1></body></html>", name.as_str()))
     }
 
+    
+#[get("/poney")]
+fn poney() -> content::Html<&'static str> {
+    content::Html("<!DOCTYPE html><html><body><h1 style=\"text-decoration: blink\">je suis un poney</h1></body></html>")
+    }
+
 fn main() {
-    rocket::ignite().mount("/", routes![hello, helloname, vntm]).launch();
+    rocket::ignite().mount("/", routes![hello, helloname, vntm, poney]).launch();
 } 
