a1051908805bea87adbf2699828705f574f8eb14
Nicolas 'Killy' Goureau <goureau.n.a@hotmail.fr>
2018-04-19 15:47:00 +0200

    Adding post init route

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,9 @@ use postgres::{Connection, TlsMode};
 use rocket::response::content;
 use rocket_contrib::{Json};
 use std::collections::LinkedList;
-use rocket::response::status;
-use rocket::Response;
-use rocket::Outcome::{Success, Failure};
-use rocket::http::Status;
 
+static X_DELTA : f32 = 0.3541;
+static Y_DELTA : f32 = 1.014;
 
 #[derive(Serialize, Deserialize)]
 struct User {
@@ -46,6 +44,13 @@ struct EnterpriseInit {
     name: String,
     longitude: f32,
     latitude: f32
+}
+
+#[derive(Serialize, Deserialize)]
+struct Position{
+    center_lat : f32,
+    center_long : f32,
+    zoom_level : i16
 }
 
 #[get("/")]
@@ -130,6 +135,56 @@ fn init() -> content::Json<String>{
     content::Json(json!({"points" : list}).to_string())
 }
 
+
+fn scale_float_add(input : f32, zoom_level : i16, is_lat : bool) -> f32 {
+    if(is_lat){
+        input + (Y_DELTA * (zoom_level as f32/10.0))
+    }else{
+        input + (X_DELTA * (zoom_level as f32/10.0))
+    }
+}
+
+fn scale_float_sup(input : f32, zoom_level : i16, is_lat : bool) -> f32 {
+    if(is_lat){
+        input - (Y_DELTA * (zoom_level as f32/10.0))
+    }else{
+        input - (X_DELTA * (zoom_level as f32/10.0))
+    }
+}
+
+#[post("/init", format="application/json", data="<input>")]
+fn init_post(input : Json<Position>) -> content::Json<String>{
+    
+    let conn = Connection::connect("postgres://killy:test123@10.44.2.8:5432/rustDb",TlsMode::None).unwrap();
+    let mut list: LinkedList<EnterpriseInit> = LinkedList::new(); 
+
+
+    println!("SELECT id_company, name, longitude, latitude 
+                FROM company WHERE (latitude > {} AND latitude < {}) 
+                AND (longitude > {} AND longitude < {})",scale_float_sup(input.center_lat, input.zoom_level, true),
+          scale_float_add(input.center_lat, input.zoom_level, true),
+          scale_float_sup(input.center_long, input.zoom_level, false),
+          scale_float_add(input.center_long, input.zoom_level, false));
+    for row in &conn.query("SELECT id_company, name, longitude, latitude 
+                FROM company WHERE (latitude > $1 AND latitude < $2) 
+                AND (longitude > $3 AND longitude < $4)",
+          &[&scale_float_sup(input.center_lat, input.zoom_level, true),
+          &scale_float_add(input.center_lat, input.zoom_level, true),
+          &scale_float_sup(input.center_long, input.zoom_level, false),
+          &scale_float_add(input.center_long, input.zoom_level, false)
+          ])
+          .unwrap() {
+        let enterprise = EnterpriseInit {
+            id: row.get(0),
+            name: row.get(1),
+            longitude : row.get(2),
+            latitude : row.get(3)
+        };
+        list.push_back(enterprise);
+    }   
+    content::Json(json!({"points" : list}).to_string())
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![hello,test_db, signin, authenticate,init]).launch();
+    rocket::ignite().mount("/", routes![hello,test_db, signin, authenticate,init, init_post]).launch();
 } 
